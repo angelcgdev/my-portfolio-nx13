5e7c4cf3a7d16a8892e65852baee76988df0d2b4
Luis Angel Caro Galoc <77682546+angelcgdev@users.noreply.github.com>
2023-05-28 10:21:24 -0500

    added dynamic experience

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -9,36 +9,35 @@ import { getDictionary } from "./dictionaries";
 import { Locale } from "./i18n-config";
 import { Toaster } from "./_components/Toaster";
 import { experienceResponse } from "./api/experience/route";
-import { experiencesEn } from "./api/db";
 
 export interface InternationlizationProps {
   params: { lang: Locale; slug?: string };
 }
 
-// async function getData(lang: Locale): Promise<experienceResponse> {
-//   const domain = process.env.HOSTNAME;
-//   const url = `${domain}/${lang}/api/experience`;
-//   console.log({url});
-//   const res = await fetch(url, {method: 'GET', headers: { 'Accept-Language': lang}});
-//   if(!res.ok) {
-//     throw new Error(`Faild to fetch data ${url}`);
-//   }
+async function getData(lang: Locale): Promise<experienceResponse> {
+  const domain = process.env.HOSTNAME;
+  const url = `${domain}/${lang}/api/experience`;
+  console.log({url});
+  const res = await fetch(url, {method: 'GET', headers: { 'Accept-Language': lang}});
+  if(!res.ok) {
+    throw new Error(`Faild to fetch data ${url}`);
+  }
   
-//   return res.json();
-// }
+  return res.json();
+}
 
 export default async function Page({
   params: { lang },
 }: InternationlizationProps) {
   const dictionary = await getDictionary(lang);
-  // const { data } = await getData(lang);
+  const { data } = await getData(lang);
 
   return (
     <>
       <Toaster />
       <Welcome dictionary={dictionary} />
       <About dictionary={dictionary} />
-      <ExperienceSection dictionary={dictionary}  experiences={experiencesEn} locale={lang}/>
+      <ExperienceSection dictionary={dictionary}  experiences={data} locale={lang}/>
       <PortfolioSection dictionary={dictionary} />
       <ContactSection dictionary={dictionary} />
     </>
